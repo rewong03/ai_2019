3a5e514e29b5f55fecf3759e25cea4351079e24f
Ryan Wong <rewong03@gmail.com>
2019-11-25 08:14:04 -0600

    Finished chp5 hw and pattern matcher

--- chp5/pattern_matcher.lisp
+++ chp5/pattern_matcher.lisp
@@ -12,6 +12,38 @@
         (t
          nil)))
 
+(defun segment-p (pat)
+  (if (listp pat)
+      (equal '?* (first pat))))
+
+(defun segment-match-helper (pat input
+                             &key (rest-input nil))
+  (let ((idx (position (second pat) input)))
+    (cond ((and (not idx)
+                (null (second pat)))
+           input)
+          ((not idx)
+           nil)
+          ((pat-match (rest pat)
+                      (subseq input idx))
+           (append rest-input
+                   (subseq input 0 idx)))
+          (t
+           (segment-match-helper pat (subseq input (+ idx 1))
+                                 :rest-input (append rest-input
+                                                     (subseq input 0 (+ idx 1))))))))
+
+(defun segment-match (pat input)
+  (let ((matched-pattern (segment-match-helper pat input)))
+    (if matched-pattern
+        (cons (second (first pat)) matched-pattern)
+        nil)))
+
+(defun match-var (pat stmt vars)
+  (or (equal stmt
+             (cdr (assoc pat vars)))
+      (null (assoc pat vars))))
+
 (defun pat-match (pat stmt
                   &key (vars nil))
   (cond ((and (null pat)
@@ -24,13 +56,31 @@
          (pat-match (rest pat)
                     (rest stmt)
                     :vars vars))
-        ((or (is-var-p (first pat))
-             (is-var-p (first stmt)))
-         (pat-match (rest pat)
-                    (rest stmt)
-                    :vars (acons (first pat)
-                                 (first stmt)
-                                  vars)))
+        ((segment-p (first pat))
+         (let ((segment (segment-match pat stmt)))
+           (if segment
+               (if (match-var (first segment) 
+                              (rest segment)
+                              vars)
+                   (if (assoc (first segment) vars)
+                       (pat-match (rest pat)
+                                  (subseq stmt (length (rest segment)))
+                                  :vars vars)
+                       (pat-match (rest pat)
+                                  (subseq stmt (length (rest segment)))
+                                  :vars (cons segment vars)))))))
+        ((is-var-p (first pat))
+         (if (match-var (first pat)
+                        (first stmt)
+                        vars)
+             (if (assoc (first pat) vars)
+                 (pat-match (rest pat) (rest stmt) :vars vars)
+                 (pat-match (rest pat)
+                            (rest stmt)
+                            :vars (acons (first pat)
+                                         (first stmt)
+                                         vars)))
+             nil))
         ((and (listp (first pat))
               (listp (first stmt)))
          (cons (pat-match (first pat)
@@ -41,10 +91,5 @@
         (t
          nil)))
 
-(defun segment-match (pat input)
-  (let ((idx (position (second pat) input)))
-    (if idx
-        (subseq input 0 idx)
-        idx)))
 
 
